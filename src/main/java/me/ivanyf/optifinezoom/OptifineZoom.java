6fa79d947978722e64550fae1f36d8c01d52c2ff
Ivan-YFw <wyf0762@foxmail.com>
2021-02-08 17:37:46 +0800

    smooth camera

--- src/main/java/me/ivanyf/optifinezoom/OptifineZoom.java
+++ src/main/java/me/ivanyf/optifinezoom/OptifineZoom.java
@@ -5,12 +5,15 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.options.StickyKeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class OptifineZoom implements ModInitializer {
-    public static boolean enable = false;
+    // control and camera to change back
+    public static boolean flag = false;
+    // control defaultFov
+    public static boolean flag1 = true;
+    public static double defaultFov = 100;
     @Override
     public void onInitialize() {
         KeyBinding key = KeyBindingHelper.registerKeyBinding(new KeyBinding(
@@ -19,12 +22,21 @@ public class OptifineZoom implements ModInitializer {
                 GLFW.GLFW_KEY_C,
                 "category.optifinezoom.optifinezoom"
         ));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            
             if (key.isPressed()) {
-                MinecraftClient.getInstance().options.fov = 30;
-            } else {
-                MinecraftClient.getInstance().options.fov = 100;
+                if (flag1) {
+                    defaultFov = client.options.fov;
+                    flag1 = false;
+                }
+                client.options.smoothCameraEnabled = true;
+                client.options.fov = 30;
+                flag = true;
+            } else if (flag) {
+                client.options.smoothCameraEnabled = false;
+                MinecraftClient.getInstance().options.fov = defaultFov;
+                flag = false;
+                flag1 = true;
             }
         });
     }
